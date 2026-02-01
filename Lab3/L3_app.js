1995bebdd6f091d255b1b78f0cea885871a515d4
rungrry2 <rungradon@gmail.com>
2026-02-01 10:57:35 +0700

    Delete Lab3 directory

--- Lab3/L3_app.js
+++
@@ -1,5 +0,0 @@
-const count = document.querySelector(".count");
-const input = document.querySelector("input");
-input.addEventListener("keyup", () => {
-    count.innerText = input.value.length;
-})
